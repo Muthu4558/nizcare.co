ae5b3a1ffbc4eaa67692bf4654c1a75ae63d7133
Muthu4558 <muthukdm45@gmail.com>
2025-10-15 15:17:44 +0530

    aerospace rework

--- client/src/components/Aerospace-sector/Footer.jsx
+++ client/src/components/Aerospace-sector/Footer.jsx
@@ -100,7 +100,7 @@ const ContactSection = () => {
 
       {/* Footer */}
       <div className="bg-[#1f2429] mt-20 py-5 text-white text-sm font-medium">
-        Nizcare | Trusted by 100+ Hotels | Health that Serves Hospitality
+        Nizcare | Trusted by 100+ Aerospace Companies | Health that Serves Hospitality
       </div>
     </section>
   );
